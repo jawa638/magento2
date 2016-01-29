9416e10792ef996989acae5e55046e24a19af765
jawa638 <jawa350638@mail.ru>
2016-01-29 16:23:22 +0200

    m2

---
+++ app/code/Agento/Test/Controller/Index/Index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: vnedviga
+ * Date: 20.01.16
+ * Time: 18:16
+ */
+
+namespace Agento\Test\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute(){
+        echo('555');
+    }
+}
